a690c674b7de1f77a503a135c69bf539056d4d33
Pieter De Clercq <pieterdeclercq@outlook.com>
2016-04-05 19:54:38 +0200

    Even shorter

--- ugent-food.py
+++ ugent-food.py
@@ -5,12 +5,10 @@ import urllib.request
 
 d = datetime.date.today()
 if len(sys.argv) == 2:
-    o = str(sys.argv[1])
+    d += datetime.timedelta(1 if sys.argv[1] == 'morgen' else 2 if sys.argv[1] == 'overmorgen' else 0)
 
-    d += datetime.timedelta(1 if o == 'morgen' else 2 if o == 'overmorgen' else 0)
-
-    if o[0:2] in ['ma', 'di', 'wo', 'do', 'vr']:
-        while d.weekday() != ['ma', 'di', 'wo', 'do', 'vr'].index(o[0:2]):
+    if sys.argv[1][0:2] in ['ma', 'di', 'wo', 'do', 'vr']:
+        while d.weekday() != ['ma', 'di', 'wo', 'do', 'vr'].index(sys.argv[1][0:2]):
             d += datetime.timedelta(1)
 
 menu = json.loads(urllib.request.urlopen(
@@ -21,8 +19,8 @@ if str(d) not in menu:
 
 print("{}[{}]{}".format("=" * 19, d, "=" * 19))
 print("{}[ SOEP ]{}".format("=" * 21, "=" * 21))
-print("* {}".format(menu[d]["soup"]["name"]))
+print("* {}".format(menu[str(d)]["soup"]["name"]))
 print("{}[ HOOFDGERECHTEN ]{}".format("=" * 16, "=" * 16))
-[print("* {}".format(g["name"])) for g in menu[d]["meat"]]
+[print("* {}".format(g["name"])) for g in menu[str(d)]["meat"]]
 print("{}[ GROENTEN ]{}".format("=" * 21, "=" * 21))
-[print("* {}".format(v)) for v in menu[d]["vegetables"]]
+[print("* {}".format(v)) for v in menu[str(d)]["vegetables"]]
