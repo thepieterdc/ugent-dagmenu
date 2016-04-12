40a874d3de764c8df0d8b75894d72cac5686e245
M1dgard <M1dgard@users.noreply.github.com>
2016-04-12 20:38:49 +0200

    Improve header generation; add comments

--- ugent-food.py
+++ ugent-food.py
@@ -5,6 +5,18 @@ import datetime
 import sys
 import urllib.request
 
+def header(text, fillchar='-', width=40):
+    tofill = max(width - 4 - len(text), 0)
+    tofill1 = tofill // 2
+    tofill2 = tofill1 + (1 if tofill % 2 else 0)
+    
+    print('{}[ {} ]{}'.format(
+        fillchar * tofill1,
+        text,
+        fillchar * tofill2,
+    ))
+
+# What day
 weekdagen = ('ma', 'di', 'wo', 'do', 'vr')
 deltas = {'morgen': 1,
           'overmorgen': 2}
@@ -17,16 +29,25 @@ if len(sys.argv) == 2:
         while d.weekday() != weekdagen.index(sys.argv[1][0:2]):
             d += datetime.timedelta(1)
 
+# Fetch from API
 menu = json.loads(urllib.request.urlopen(
     "http://zeus.ugent.be/hydra/api/1.0/resto/week/{}.json".format(d.isocalendar()[1])).read().decode('utf-8'))
 
+# Get wanted day in API response
 if str(d) not in menu:
     exit("Restaurant gesloten")
+today = menu[str(d)]
 
-print("{}[{}]{}".format("=" * 19, d, "=" * 19))
-print("{}[ SOEP ]{}".format("=" * 21, "=" * 21))
-print("* {}".format(menu[str(d)]["soup"]["name"]))
-print("{}[ HOOFDGERECHTEN ]{}".format("=" * 16, "=" * 16))
-[print("* {}".format(g["name"])) for g in menu[str(d)]["meat"]]
-print("{}[ GROENTEN ]{}".format("=" * 21, "=" * 21))
-[print("* {}".format(v)) for v in menu[str(d)]["vegetables"]]
+# Print menu
+header(str(d), fillchar='=')
+
+header('SOEP')
+print("* {}".format(today["soup"]["name"]))
+
+header('HOOFDGERECHTEN')
+for g in today["meat"]:
+    print("* {}".format(g["name"]))
+
+header('GROENTEN')
+for v in today["vegetables"]:
+    print("* {}".format(v))
