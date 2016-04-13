ad1e85219b6143113761f1e2b95e1771bd62c5dc
Pieter De Clercq <pieterdeclercq@outlook.com>
2016-04-13 19:14:46 +0200

    Api call fixed #4

--- ugent-food.py
+++ ugent-food.py
@@ -4,6 +4,8 @@ import json
 import datetime
 import sys
 import urllib.request
+from urllib.error import HTTPError
+
 
 def header(text, fillchar='-', width=40):
     tofill = max(width - 4 - len(text), 0)
@@ -14,6 +16,7 @@ def header(text, fillchar='-', width=40):
         text,
         fillchar * (tofill - leftpad),
     ))
+
 
 # What day
 weekdagen = ('ma', 'di', 'wo', 'do', 'vr')
@@ -29,24 +32,22 @@ if len(sys.argv) == 2:
             d += datetime.timedelta(1)
 
 # Fetch from API
-menu = json.loads(urllib.request.urlopen(
-    "http://zeus.ugent.be/hydra/api/1.0/resto/week/{}.json".format(d.isocalendar()[1])).read().decode('utf-8'))
+try:
+    menu = json.loads(urllib.request.urlopen(
+        "http://zeus.ugent.be/hydra/api/2.0/resto/menu/nl/{}/{}/{}.json".format(d.year, d.month, d.day)).read().decode(
+        'utf-8'))
+    # Print menu
+    header(str(d), fillchar='=')
 
-# Get wanted day in API response
-if str(d) not in menu:
+    header('SOEP')
+    print("* {}".format(today["soup"]["name"]))
+
+    header('HOOFDGERECHTEN')
+    for g in today["meat"]:
+        print("* {}".format(g["name"]))
+
+    header('GROENTEN')
+    for v in today["vegetables"]:
+        print("* {}".format(v))
+except HTTPError:
     exit("Restaurant gesloten")
-today = menu[str(d)]
-
-# Print menu
-header(str(d), fillchar='=')
-
-header('SOEP')
-print("* {}".format(today["soup"]["name"]))
-
-header('HOOFDGERECHTEN')
-for g in today["meat"]:
-    print("* {}".format(g["name"]))
-
-header('GROENTEN')
-for v in today["vegetables"]:
-    print("* {}".format(v))
