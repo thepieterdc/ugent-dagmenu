b08abbb477424ea6a2e8dae68d9fd73b444cba76
Pieter De Clercq <pieterdeclercq@outlook.com>
2018-11-22 12:18:32 +0100

    Add notifier

---
+++ notifier.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import json
+import datetime
+import os
+
+import requests
+import sys
+from urllib.error import HTTPError
+
+
+d = datetime.date.today()
+
+# Fetch from API
+try:
+    menu = requests.get("http://zeus.ugent.be/hydra/api/2.0/resto/menu/nl/{}/{}/{}.json".format(d.year, d.month, d.day)).json()
+    print(menu)
+
+    soups = set()
+    mains = set()
+
+    for s in menu["meals"]:
+        if s["kind"] == "soup":
+            soups.add(s["name"].replace(" klein", "").replace(" groot", ""))
+        if s["type"] == "main" and s["kind"] != "soup":
+            mains.add(s["name"])
+
+    os.system("notify-send -i restaurant --urgency=critical \"Soep\" \"{}\"".format(" en ".join(soups)))
+    os.system("notify-send -i restaurant --urgency=critical \"Hoofdgerechten\" \"{}\"".format(", ".join(mains)))
+except HTTPError:
+    # don't do anything
+    pass
